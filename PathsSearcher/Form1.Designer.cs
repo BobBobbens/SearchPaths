ede667171dd4bb4e417fbe108096d56f0ba5ab39
BobBobbens <87965558+BobBobbens@users.noreply.github.com>
2021-08-23 19:12:17 +0300

    Add files via upload

---
+++ PathsSearcher/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace PathsSearcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBoxKey = new System.Windows.Forms.TextBox();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.ButtonStart = new System.Windows.Forms.Button();
+            this.ButtonReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Выберите диск:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(289, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Укажите ключевое слово:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(349, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Укажите путь директории, в которой будет осуществляться поиск:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(289, 257);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(176, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "*Поля можно оставлять пустыми";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(28, 44);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(89, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBoxKey
+            // 
+            this.textBoxKey.Location = new System.Drawing.Point(292, 45);
+            this.textBoxKey.Name = "textBoxKey";
+            this.textBoxKey.Size = new System.Drawing.Size(171, 20);
+            this.textBoxKey.TabIndex = 5;
+            this.textBoxKey.TextChanged += new System.EventHandler(this.textBoxKey_TextChanged);
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(28, 137);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(435, 20);
+            this.textBoxPath.TabIndex = 6;
+            this.textBoxPath.TextChanged += new System.EventHandler(this.textBoxPath_TextChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(28, 199);
+            this.progressBar1.MarqueeAnimationSpeed = 10;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(435, 18);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar1.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Например: C:\\";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(289, 68);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(121, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Например: 06_Выпуск";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(25, 173);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(191, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Например: C:\\Program Files\\Microsoft";
+            // 
+            // ButtonStart
+            // 
+            this.ButtonStart.Location = new System.Drawing.Point(28, 247);
+            this.ButtonStart.Name = "ButtonStart";
+            this.ButtonStart.Size = new System.Drawing.Size(84, 23);
+            this.ButtonStart.TabIndex = 11;
+            this.ButtonStart.Text = "Запустить";
+            this.ButtonStart.UseVisualStyleBackColor = true;
+            this.ButtonStart.Click += new System.EventHandler(this.ButtonStart_Click);
+            // 
+            // ButtonReset
+            // 
+            this.ButtonReset.Location = new System.Drawing.Point(141, 247);
+            this.ButtonReset.Name = "ButtonReset";
+            this.ButtonReset.Size = new System.Drawing.Size(100, 23);
+            this.ButtonReset.TabIndex = 12;
+            this.ButtonReset.Text = "Сбросить всё";
+            this.ButtonReset.UseVisualStyleBackColor = true;
+            this.ButtonReset.Click += new System.EventHandler(this.ButtonReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 291);
+            this.Controls.Add(this.ButtonReset);
+            this.Controls.Add(this.ButtonStart);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.textBoxKey);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBoxKey;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button ButtonStart;
+        private System.Windows.Forms.Button ButtonReset;
+    }
+}
+
---
+++ PathsSearcher/Form1.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PathsSearcher
+{
+    public partial class Form1 : Form
+    {
+        static string[] drives = System.Environment.GetLogicalDrives();
+        static string pathfile = Application.StartupPath.ToString() + @"DirPaths.txt";
+
+        private static string keyWord;
+        private static string pathFind;
+        private string drive;
+        static System.Collections.Specialized.StringCollection log = new System.Collections.Specialized.StringCollection();
+
+
+        static void WalkDirectoryTree(System.IO.DirectoryInfo root)
+        {
+
+            System.IO.DirectoryInfo[] files = null;
+            System.IO.DirectoryInfo[] subDirs = null;
+
+            try
+            {
+                files = root.GetDirectories(keyWord);
+            }
+
+            catch (UnauthorizedAccessException e)
+            {
+
+                log.Add(e.Message);
+            }
+
+            catch (System.IO.DirectoryNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            if (files != null)
+            {
+                foreach (System.IO.DirectoryInfo fi in files)
+                {
+                    using (FileStream fs = new FileStream(pathfile, FileMode.OpenOrCreate, FileAccess.Write))
+                    {
+                        fs.Seek(0, SeekOrigin.End);
+                        using (StreamWriter writer = new StreamWriter(fs))
+                        {
+                            writer.WriteLine(fi.FullName);
+                        }
+                    }
+
+                }
+
+                
+                try
+                {
+                    subDirs = root.GetDirectories();
+                }
+
+                catch (UnauthorizedAccessException e)
+                {
+
+                    log.Add(e.Message);
+                }
+
+                catch (System.IO.DirectoryNotFoundException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+                foreach (System.IO.DirectoryInfo dirInfo in subDirs)
+                {
+                    WalkDirectoryTree(dirInfo);
+                }
+            }
+        }
+
+        static void WalkDirectoryTreeFile(System.IO.DirectoryInfo root)
+        {
+            System.IO.FileInfo[] files = null;
+            System.IO.DirectoryInfo[] subDirs = null;
+
+
+            try
+            {
+                if ((keyWord == ".jpg") || (keyWord == ".pdf") || (keyWord == ".exe")
+                || (keyWord == ".zip") || (keyWord == ".txt") || (keyWord == ".lnk")
+                || (keyWord == ".json") || (keyWord == ".xls") || (keyWord == ".rar")
+                || (keyWord == ".img") || (keyWord == ".mp4") || (keyWord == ".docx")
+                || (keyWord == ".dat") || (keyWord == ".rtf") || (keyWord == ".html")
+                || (keyWord == ".exl") || (keyWord == ".dll")
+                    ) { keyWord = "*" + keyWord; }
+                files = root.GetFiles(keyWord);
+            }
+
+            catch (UnauthorizedAccessException e)
+            {
+
+                log.Add(e.Message);
+            }
+
+            catch (System.IO.DirectoryNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            if (files != null)
+            {
+                foreach (System.IO.FileInfo fi in files)
+                {
+
+                    using (FileStream fs = new FileStream(pathfile, FileMode.OpenOrCreate, FileAccess.Write))
+                    {
+                        fs.Seek(0, SeekOrigin.End);
+                        using (StreamWriter writer = new StreamWriter(fs))
+                        {
+                            writer.WriteLine(fi.FullName);
+                        }
+                    }
+                }
+
+
+                subDirs = root.GetDirectories();
+
+                foreach (System.IO.DirectoryInfo dirInfo in subDirs)
+                {
+
+                    WalkDirectoryTreeFile(dirInfo);
+                }
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxKey_TextChanged(object sender, EventArgs e)
+        {
+             keyWord = textBoxKey.Text;
+        }
+
+        private void textBoxPath_TextChanged(object sender, EventArgs e)
+        {
+             pathFind = textBoxPath.Text;
+        }
+
+        private void ButtonStart_Click(object sender, EventArgs e)
+        {
+            if (keyWord == null)
+            {
+                keyWord = "";
+            }
+            if (pathFind == null)
+            {
+                pathFind = "";
+            }
+            if (drive == null)
+            {
+                drive = "";
+            }
+            if (drive == "" && pathFind == "")
+            {
+                MessageBox.Show("Укажите путь или диск");
+            }
+            else
+            {
+                BackgroundWorker bw;
+                progressBar1.Visible = true;
+                bw = new BackgroundWorker();
+                bw.DoWork += (obj, ea) => TasksAsync(1);
+                bw.RunWorkerAsync();
+            }
+        }
+        private async void TasksAsync(int times)
+        {
+
+            if (drive != "" && pathFind != "")
+            {
+                string path = pathFind;
+                DirectoryInfo di = new DirectoryInfo(path);
+                if (keyWord.Contains('.')) { WalkDirectoryTreeFile(di); }
+
+                WalkDirectoryTree(di);
+
+
+            }
+            else if (drive != "")
+            {
+                DriveInfo di = new DriveInfo(drive);
+
+                if (!di.IsReady)
+                {
+                    MessageBox.Show("The drive {0} could not be read", di.Name);
+                    Application.Exit();
+                }
+                DirectoryInfo rootDir = di.RootDirectory;
+                if (keyWord.Contains('.')) { WalkDirectoryTreeFile(rootDir); }
+
+                WalkDirectoryTree(rootDir);
+
+            }
+
+            else
+            {
+                string path = pathFind;
+                DirectoryInfo di = new DirectoryInfo(path);
+                if (keyWord.Contains('.')) { WalkDirectoryTreeFile(di); }
+
+                WalkDirectoryTree(di);
+
+            }
+            MessageBox.Show(
+                "Выполнено",
+                "Сообщение",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.None,
+                MessageBoxDefaultButton.Button1,
+                MessageBoxOptions.DefaultDesktopOnly);
+
+            Application.Exit();
+        }
+        private void ButtonReset_Click(object sender, EventArgs e)
+        {
+            keyWord = "";
+            drive = "";
+            pathFind = "";
+            textBoxKey.Clear();
+            comboBox1.ResetText();
+            textBoxPath.Clear();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            drive = comboBox1.SelectedItem.ToString();
+        }
+
+        private async void  Form1_Load(object sender, EventArgs e)
+        {
+             progressBar1.Visible = false;
+
+            if (File.Exists(pathfile))
+            {
+                File.Delete(pathfile);
+            }
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+
+
+
+            for (int i = 0; i < drives.Length; i++)
+            {
+                comboBox1.Items.Insert(i, drives[i]);
+            }
+            comboBox1.Items.Insert(drives.Length, "");
+        }
+    }
+}
